e79f67f0031619a764fce9465a4246f111bd7f9a
hitesh <hiteshtripathi45@gmail.com>
2025-02-04 23:56:06 +0530

    data is render in cv.jsx

--- src/component/cv.jsx
+++ src/component/cv.jsx
@@ -3,15 +3,17 @@
 
 import React from 'react'
 
-const Cv = ({formdata}) => {
+const Cv = ({ formdata }) => {
+  { formdata }
   return (
-    <>
-    <div>{formdata.name}</div>
-    <div>{formdata.email}</div>
-    <div>{formdata.phone}</div>
-    <div>{formdata.surname}</div>
-    <div>{formdata.summary}</div>
-    </>
+      <div className='cvv'>
+        <div className='rec'><div className='first'> <h1 className='title'>{formdata.name}{formdata.surname}</h1></div> <div className='second'>{formdata.profile}</div></div>
+        <div className='rec'><div className='first'>{formdata.summary}</div> <div className='second'><h1>{formdata.email}</h1><h1>{formdata.phone}</h1></div></div>
+        <div className='rec'><div className='first'><h1>{formdata.work.company}</h1> <h3>{formdata.work.start}{formdata.work.end}</h3><h2>{formdata.work.position}</h2></div><div className='second'></div></div>
+        <div className='rec'><div className='first'></div><div className='second'><h1>{formdata.education.degree}</h1> <h1>{formdata.education.collage}</h1><h3>{formdata.education.pass}</h3></div></div>
+      </div>
+
+
   )
 }
 
